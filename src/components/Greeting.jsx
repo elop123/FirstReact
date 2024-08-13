6833db3dfd26fef240fcfe9b0431a36e0b77b336
elop123 <142408289+elop123@users.noreply.github.com>
2024-08-13 09:53:12 +0200

    props

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -3,7 +3,7 @@ import style from '../styles/Greeting.module.scss'
 function Greeting (props){
     return(
      <>
-     <h1 className={style.fontStyle}>Welcome <span>{props.name}</span> !</h1>
+     <h1 className={style.fontStyle}>Welcome <span>{props.name}</span>!</h1>
      </>
    )
 }
